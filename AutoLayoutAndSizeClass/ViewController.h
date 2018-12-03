1ce144982585cbd6bd1425f5a86163613ddf44ea
four <657585618@qq.com>
2018-12-03 17:20:10 +0800

    Initial Commit

---
+++ AutoLayoutAndSizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutAndSizeClass
+//
+//  Created by four on 2018/12/3.
+//  Copyright © 2018 four. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutAndSizeClass/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AutoLayoutAndSizeClass
+//
+//  Created by four on 2018/12/3.
+//  Copyright © 2018 four. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
